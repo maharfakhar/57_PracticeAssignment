50980b9ec9263377c29057f434cbb44d032d2282
maharfakhar <148197322+maharfakhar@users.noreply.github.com>
2023-10-26 17:00:03 +0500

    Create Question2.cs

---
+++ Fakhar/Practice Assignment/Lab01/Question2.cs
@@ -0,0 +1,37 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (IsPrime(number))
+        {
+            Console.WriteLine($"{number} is a prime number.");
+        }
+        else
+        {
+            Console.WriteLine($"{number} is not a prime number.");
+        }
+    }
+
+    static bool IsPrime(int num)
+    {
+        if (num < 2)
+        {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.Sqrt(num); i++)
+        {
+            if (num % i == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
