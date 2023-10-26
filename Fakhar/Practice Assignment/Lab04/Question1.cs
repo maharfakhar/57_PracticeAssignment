8ed596e6f83e4171d1e21f171fb442402800ba56
maharfakhar <148197322+maharfakhar@users.noreply.github.com>
2023-10-26 17:12:35 +0500

    Create Question1.cs

---
+++ Fakhar/Practice Assignment/Lab04/Question1.cs
@@ -0,0 +1,11 @@
+// File: Program.cs
+class Program
+{
+    static void Main()
+    {
+        Person person = new Person();
+        person.FirstName = "John";
+        person.LastName = "Doe";
+        person.Display(); // Output: Name: John Doe
+    }
+}
